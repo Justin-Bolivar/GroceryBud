22065865b595dc7cef0f00a60c0f71e0e7c13053
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 14:04:20 +0800

    Functionality to Submit

--- src/Form.tsx
+++ src/Form.tsx
@@ -17,5 +17,23 @@ const Form: React.FC<FormProps> = ({ addItem }) => {
     setNewItemName('');
   };
 
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>grocery bud</h4>
+      <div className='form-control'>
+        <input
+          type='text'
+          className='form-input'
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type='submit' className='btn'>
+          add item
+        </button>
+      </div>
+    </form>
+  );
+};
+
 
 export default Form;
