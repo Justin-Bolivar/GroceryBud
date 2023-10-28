23cf95262cb94ea69eb509b40452c7724288a324
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 13:47:04 +0800

    Initial Layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,19 @@ function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div id="root">
+      <section className="section-center">
+        <div className="toastify"></div>
+        <form>
+          <h4>Grocery Bud</h4>
+          <div className="form-control">
+            <input type="text" className="form-control"></input>
+            <button type ="submit" className="btn">Add Item</button>
+          </div>
+        </form>
+        <div className = "items"></div>
+      </section>
+    </div>
   )
 }
 
