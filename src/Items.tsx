3d4e8f33e64be9f126334c1e0aa6d836b8ad84bf
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 13:56:15 +0800

    imported singleItem to items to connect

--- src/Items.tsx
+++ src/Items.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import SingleItem from './SingleItem';
 
 interface Item {
   id: string;
@@ -7,4 +7,26 @@ interface Item {
   completed: boolean;
 }
 
+interface ItemsProps {
+    items: Item[];
+    removeItem: (itemId: string) => void;
+    editItem: (itemId: string) => void;
+  }
+
+  const Items: React.FC<ItemsProps> = ({ items, removeItem, editItem }) => {
+    return (
+      <div className='items'>
+        {items.map((item: Item) => {
+          return (
+            <SingleItem
+              key={item.id}
+              item={item}
+              removeItem={removeItem}
+              editItem={editItem}
+            />
+          );
+        })}
+      </div>
+    );
+  };
 export default Items;
