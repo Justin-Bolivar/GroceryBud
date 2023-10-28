32b140acd6f686ec2d7b954c3c38e38152cdd332
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 14:09:51 +0800

    Functional remove and Item storing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import './App.css'
+import React, { useState } from 'react'; // Import the Items component
+import { nanoid } from 'nanoid';
+import { ToastContainer, toast } from 'react-toastify';
 
 
 interface Item {
@@ -21,6 +23,40 @@ const setLocalStorage = (items: Item[]) => {
 }
 
 const defaultList: Item[] = getLocalStorage();
+
+const App: React.FC = () => {
+  const [items, setItems] = useState<Item[]>(defaultList);
+
+  const addItem = (itemName: string) => {
+    const newItem: Item = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success('item added to the list');
+  }
+
+  const removeItem = (itemId: string) => {
+    const newItems = items.filter((item) => item.id !== itemId);
+    setItems(newItems);
+    setLocalStorage(newItems);
+    toast.success('item deleted');
+  }
+
+  const editItem = (itemId: string) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem: Item = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+  }
 
 function App() {
   return (
