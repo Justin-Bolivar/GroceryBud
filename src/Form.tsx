a4ced25cac32c872c96916e0a03b47398db3d386
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 14:01:08 +0800

    added functionality to submit and toastify

---
+++ src/Form.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { toast } from 'react-toastify';
+
+interface FormProps {
+  addItem: (newItemName: string) => void;
+}
+const Form: React.FC<FormProps> = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!newItemName) {
+      toast.error('please provide value');
+      return;
+    }
+    addItem(newItemName);
+    setNewItemName('');
+  };
