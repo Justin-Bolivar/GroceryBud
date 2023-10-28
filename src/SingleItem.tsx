ff81e9b1b443053aeac169a28ea437054464b5a6
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2023-10-28 13:53:15 +0800

    Single Item checkbox and adde button, capitalize

--- src/SingleItem.tsx
+++ src/SingleItem.tsx
@@ -10,4 +10,32 @@ interface SingleItemProps {
   editItem: (itemId: string) => void;
 }
 
+const SingleItem: React.FC<SingleItemProps> = ({ item, removeItem, editItem }) => {
+    return (
+      <div className='single-item'>
+        <input
+          type='checkbox'
+          checked={item.completed}
+          onChange={() => editItem(item.id)}
+        />
+        <p
+          style={{
+            textTransform: 'capitalize',
+            textDecoration: item.completed ? 'line-through' : 'none',
+          }}
+        >
+          {item.name}
+        </p>
+        <button
+          className='btn remove-btn'
+          type='button'
+          onClick={() => removeItem(item.id)}
+        >
+          delete
+        </button>
+      </div>
+    );
+  };
+  
+
 export default SingleItem;
